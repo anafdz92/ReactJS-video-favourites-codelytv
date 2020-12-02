0ad191731610ada527510de7d639101ead47e315
Ana Fernandez <anafdz92@gmail.com>
2020-12-02 19:00:39 +0100

    docs

---
+++ docs/build/precache-manifest.ea66ad652193835dc7caecb029814236.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fb32b63bb565fe1e43b1",
+    "url": "/static/css/main.a11d8deb.chunk.css"
+  },
+  {
+    "revision": "fb32b63bb565fe1e43b1",
+    "url": "/static/js/main.fb32b63b.chunk.js"
+  },
+  {
+    "revision": "955baaeb14dff255f58f",
+    "url": "/static/js/1.955baaeb.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d88738c2e0b9ecc42bc28bace6ce25d",
+    "url": "/index.html"
+  }
+];
