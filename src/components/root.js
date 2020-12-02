7d7596886558cd231700f6193011462223c1ef57
Ana Fernandez <anafdz92@gmail.com>
2020-12-02 20:34:21 +0100

    hashrouter

--- src/components/root.js
+++ src/components/root.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom'
+import { HashRouter, Route } from 'react-router-dom'
 import List from './List';
 import Detail from './Detail';
 
 const Root = () => (
-	<BrowserRouter>
+	<HashRouter>
 		<div>
 			<Route exact path="/" component={List}/>
 			<Route exact path="/:id" component={Detail}/>
 		</div>
-	</BrowserRouter>
+	</HashRouter>
 );
 
 export default Root;
