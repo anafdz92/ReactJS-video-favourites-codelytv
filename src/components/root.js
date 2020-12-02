794eba6989ca3b0042b20682e3d0889dfa2ff17c
Ana Fernandez <anafdz92@gmail.com>
2020-12-02 20:38:19 +0100

    switch

--- src/components/root.js
+++ src/components/root.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { HashRouter, Route } from 'react-router-dom'
+import { HashRouter, Route, Switch } from 'react-router-dom'
 import List from './List';
 import Detail from './Detail';
 
 const Root = () => (
 	<HashRouter>
-		<div>
+		<Switch>
 			<Route exact path="/" component={List}/>
 			<Route exact path="/:id" component={Detail}/>
-		</div>
+		</Switch>
 	</HashRouter>
 );
 
